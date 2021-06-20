3e35d7457493365e2cae837dd434961abc702f73
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-06-20 10:41:51 -0300

    Install sass-loader and node-sass

--- frontend/src/pages/Login/LoginComponent.vue
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="login">
-      <p>Login do sistema</p>
+      <div class="info">
+          <h1 class="title-login">Título</h1>
+          <p>Login do sistema</p>
+      </div>
+      <div class="footer">
+          <h1>Rodapé</h1>
+      </div>
   </div>
 </template>
 
@@ -10,10 +16,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.login{
-    width: 100%;
-    height: 100vh;
-    background-color: #069;
-}
-</style>
+<style lang="scss" src="./style.scss" scoped />
