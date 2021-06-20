2994f8bb7c0c5ddddc4d75862317713fbeb00b67
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-06-20 00:04:52 -0300

    Create routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/login', component: LoginComponent},
+  {path: '/home', component: HomeComponent},
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
